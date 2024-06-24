c5792b0bf7d1d8f28d6d74a658535533570139b2
Azim Baghadiya <azimsb120@gmail.com>
2024-06-24 17:29:59 -0400

    feat: implement POST patient API in backend

--- backend/main.py
+++ backend/main.py
@@ -28,3 +28,14 @@ patient_data.columns = [col.replace(" ", "_") for col in patient_data.columns]
 @app.get("/patients")
 def get_patients():
     return patient_data.to_dict(orient="records")
+
+@app.post("/patients")
+def add_patient(record: PatientRecord):
+    global patient_data
+    # pydantic model to python dict
+    record_dict = record.model_dump()
+    # append row by making a new df and concatinating both
+    new_row = pd.DataFrame([record_dict])
+    patient_data = pd.concat([patient_data, new_row], ignore_index=True)
+    # return the new record
+    return record_dict
