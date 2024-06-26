19e064fa540845c3e28727e0db6fe71147adc365
Azim Baghadiya <azimsb120@gmail.com>
2024-06-26 05:06:02 -0400

    feat: improved pagination and filtering in react app

--- frontend/src/PatientDashboard.js
+++ frontend/src/PatientDashboard.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import Select from "react-select";
+import { ClipLoader } from "react-spinners";
 import "./index.css";
 
 const PatientDashboard = () => {
@@ -18,28 +19,29 @@ const PatientDashboard = () => {
   const backendUrl =
     "https://m92nc6tvoe.execute-api.us-east-2.amazonaws.com/Prod";
 
+  // Column names mapping
   const headers = [
-    "Name",
-    "Age",
-    "Gender",
-    "Blood Type",
-    "Medical Condition",
-    "Date of Admission",
-    "Doctor",
-    "Hospital",
-    "Insurance Provider",
-    "Billing Amount",
-    "Room Number",
-    "Admission Type",
-    "Discharge Date",
-    "Medication",
-    "Test Results",
+    { display: "Name", api: "Name" },
+    { display: "Age", api: "Age" },
+    { display: "Gender", api: "Gender" },
+    { display: "Blood Type", api: "Blood_Type" },
+    { display: "Medical Condition", api: "Medical_Condition" },
+    { display: "Date of Admission", api: "Date_of_Admission" },
+    { display: "Doctor", api: "Doctor" },
+    { display: "Hospital", api: "Hospital" },
+    { display: "Insurance Provider", api: "Insurance_Provider" },
+    { display: "Billing Amount", api: "Billing_Amount" },
+    { display: "Room Number", api: "Room_Number" },
+    { display: "Admission Type", api: "Admission_Type" },
+    { display: "Discharge Date", api: "Discharge_Date" },
+    { display: "Medication", api: "Medication" },
+    { display: "Test Results", api: "Test_Results" },
   ];
   const cellClass = "py-3 px-6 text-left whitespace-nowrap";
 
   const columnOptions = headers.map((header) => ({
-    value: header,
-    label: header,
+    value: header.api,
+    label: header.display,
   }));
 
   useEffect(() => {
@@ -49,23 +51,29 @@ const PatientDashboard = () => {
   const fetchPatients = (page) => {
     setLoading(true);
     fetch(`${backendUrl}/patients?page=${page}&size=${pageSize}`)
-      .then((response) => response.json())
+      .then((response) => {
+        return response.json();
+      })
       .then((data) => {
         setPatients(data.data);
-        setFilteredPatients(data.data);
+        setFilteredPatients(filterPatients(search, searchColumns, data.data));
         setTotal(data.total);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
         setLoading(false);
       });
   };
 
-  const filterPatients = (query, columns) => {
+  const filterPatients = (query, columns, patientsList) => {
     if (query === "") {
-      return patients;
+      return patientsList;
     } else {
-      return patients.filter((patient) =>
+      return patientsList.filter((patient) =>
         columns.some((column) => {
           const fieldValue = String(patient[column.value]).toLowerCase();
-          if (column.value === "Gender") {
+          if (column.value === "Gender" || column.value === "Blood_Type") {
             return fieldValue === query.toLowerCase();
           }
           return fieldValue.includes(query.toLowerCase());
@@ -77,17 +85,15 @@ const PatientDashboard = () => {
   const handleSearch = (e) => {
     const query = e.target.value;
     setSearch(query);
-    setFilteredPatients(filterPatients(query, searchColumns));
+    setFilteredPatients(filterPatients(query, searchColumns, patients));
   };
 
   const handleColumnChange = (selectedOptions) => {
     setSearchColumns(selectedOptions || []);
-    setFilteredPatients(filterPatients(search, selectedOptions || []));
+    setFilteredPatients(
+      filterPatients(search, selectedOptions || [], patients)
+    );
   };
-
-  useEffect(() => {
-    setFilteredPatients(filterPatients(search, searchColumns));
-  }, [searchColumns]);
 
   const handleNextPage = () => {
     if (page * pageSize < total) {
@@ -134,59 +140,64 @@ const PatientDashboard = () => {
           className="w-full"
         />
       </div>
-      {loading ? (
-        <p className="text-center">Loading...</p>
-      ) : (
-        <>
-          <div className="overflow-x-auto max-h-[calc(100vh-20rem)] overflow-auto mb-4">
-            <table className="min-w-full bg-white border border-gray-300 rounded-lg shadow">
-              <thead className="sticky top-0 bg-white">
+      <div className="relative">
+        {loading && (
+          <div className="absolute inset-0 flex justify-center items-center bg-white bg-opacity-75">
+            <ClipLoader size={50} color={"#007AC2"} loading={loading} />
+          </div>
+        )}
+        <div
+          className={`overflow-x-auto max-h-[calc(100vh-20rem)] overflow-auto mb-4 ${
+            loading ? "opacity-50" : ""
+          }`}
+        >
+          <table className="min-w-full bg-white border border-gray-300 rounded-lg shadow">
+            <thead className="sticky top-0 bg-white">
+              <tr
+                className="uppercase text-sm leading-normal"
+                style={{ backgroundColor: "#007AC2", color: "white" }}
+              >
+                {headers.map((header) => (
+                  <th key={header.api} className={cellClass}>
+                    {header.display}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody className="text-gray-600 text-sm font-light">
+              {filteredPatients.map((patient) => (
                 <tr
-                  className="uppercase text-sm leading-normal"
-                  style={{ backgroundColor: "#007AC2", color: "white" }}
+                  key={patient.Name}
+                  className="border-b border-gray-200 hover:bg-gray-100 hover-highlight"
                 >
-                  {headers.map((header) => (
-                    <th key={header} className={cellClass}>
-                      {header}
-                    </th>
+                  {headers.map((header, i) => (
+                    <td key={i} className={cellClass}>
+                      {patient[header.api]}
+                    </td>
                   ))}
                 </tr>
-              </thead>
-              <tbody className="text-gray-600 text-sm font-light">
-                {filteredPatients.map((patient) => (
-                  <tr
-                    key={patient.Name}
-                    className="border-b border-gray-200 hover:bg-gray-100 hover-highlight"
-                  >
-                    {Object.values(patient).map((value, i) => (
-                      <td key={i} className={cellClass}>
-                        {value}
-                      </td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <div className="flex justify-between items-center mt-4">
-            <button
-              onClick={handlePreviousPage}
-              disabled={page === 1}
-              className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg focus:outline-none"
-            >
-              Previous
-            </button>
-            <span>Page {page}</span>
-            <button
-              onClick={handleNextPage}
-              disabled={page * pageSize >= total}
-              className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg focus:outline-none"
-            >
-              Next
-            </button>
-          </div>
-        </>
-      )}
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="flex justify-between items-center mt-4">
+        <button
+          onClick={handlePreviousPage}
+          disabled={page === 1}
+          className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg focus:outline-none"
+        >
+          Previous
+        </button>
+        <span>Page {page}</span>
+        <button
+          onClick={handleNextPage}
+          disabled={page * pageSize >= total}
+          className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg focus:outline-none"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
