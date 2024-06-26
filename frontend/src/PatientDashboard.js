c1e3df4b7a037d5b4306f6613cc0ecbeb2ce100d
Azim Baghadiya <azimsb120@gmail.com>
2024-06-25 22:51:07 -0400

    feat: add search to react app

--- frontend/src/PatientDashboard.js
+++ frontend/src/PatientDashboard.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
 import "./index.css";
 
 const PatientDashboard = () => {
   const [patients, setPatients] = useState([]);
+  const [filteredPatients, setFilteredPatients] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
   const headers = [
     "Name",
     "Age",
@@ -30,17 +33,45 @@ const PatientDashboard = () => {
       .then((response) => response.json())
       .then((data) => {
         setPatients(data);
+        setFilteredPatients(data);
         setLoading(false);
       });
   }, []);
 
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    if (e.target.value === "") {
+      setFilteredPatients(patients);
+    } else {
+      setFilteredPatients(
+        patients.filter((patient) =>
+          Object.values(patient).some((value) =>
+            String(value).toLowerCase().includes(e.target.value.toLowerCase())
+          )
+        )
+      );
+    }
+  };
+
   return (
     <div className="container mx-auto mt-5 p-4">
       <h1 className="text-2xl font-bold mb-4 text-center">Patient Dashboard</h1>
+      <div className="relative mb-4">
+        <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
+          <FaSearch className="text-gray-500" />
+        </span>
+        <input
+          type="text"
+          value={search}
+          onChange={handleSearch}
+          placeholder="Search..."
+          className="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
       {loading ? (
         <p className="text-center">Loading...</p>
       ) : (
-        <div className="overflow-x-auto">
+        <div className="overflow-x-auto max-h-[calc(100vh-10rem)] overflow-auto">
           <table className="min-w-full bg-white border border-gray-300 rounded-lg shadow">
             <thead>
               <tr className="bg-blue-500 text-white uppercase text-sm leading-normal">
@@ -52,7 +83,7 @@ const PatientDashboard = () => {
               </tr>
             </thead>
             <tbody className="text-gray-600 text-sm font-light">
-              {patients.map((patient) => (
+              {filteredPatients.map((patient) => (
                 <tr
                   key={patient.Name}
                   className="border-b border-gray-200 hover:bg-gray-100"
